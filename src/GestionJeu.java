fd33e748ab2b1f5872a4b16da44600f8ac34cb3d
MARMION Steven <o22202479@info03-22.iut45.univ-orleans.fr>
2023-04-12 09:27:42 +0200

    rangement .class dans bin

--- src/GestionJeu.java
+++ src/GestionJeu.java
@@ -58,7 +58,7 @@ public class GestionJeu {
         /* Fin bonus */
     }
     public int getHauteur() {
-        return 30;
+        return 60;
     }
     public int getLargeur() {
         return 100;
