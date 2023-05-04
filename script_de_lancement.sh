6b688c3ce027b1986bb908919e4299b211d7944c
Steven <marmionsteven8@gmail.com>
2023-05-04 21:32:08 +0200

    ajout de la ligne de création de documentation dans le script de lancement et de compilation et ajout de la nouvelle documentation de tout le code

--- script_de_lancement.sh
+++ script_de_lancement.sh
@@ -7,6 +7,8 @@ javac --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls -d bin
 if [ $? -eq 0 ]; then
   # Compile l'Executable du dossier bin
   java --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls -cp bin Executable
+  # créer la doc de la nouvelle version 
+  javadoc -d doc/ src/*.java --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls
 else
   # Si la compilation échoue, nous renvoyons le message suivant :
   echo "La compilation a échoué, regardez les messages d'erreurs ce-dessus"
